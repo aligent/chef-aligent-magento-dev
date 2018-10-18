bb1db2912cc852bc4c0d2901656486236b59e573
Greg Work <greg.work@aligent.com.au>
2018-10-18 12:22:11 +1030

    Bump version

--- metadata.rb
+++ metadata.rb
@@ -4,7 +4,7 @@ maintainer_email 'sysadmin@aligent.com.au'
 license          'MIT'
 description      'Custom recipes for an Aligent dev environment'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          '0.2.1'
+version          '0.2.2'
 
 depends 'mysql2_chef_gem', '~> 1.1.0'
 depends 'database', '~> 4.0.6'
